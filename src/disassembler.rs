dd9ae7f93350d3931bb4d0ca197ee170893bb2d0
Valentin B <valentin.be@protonmail.com>
2020-06-04 17:05:20 +0200

    Add support for Falcon operand sizes

--- src/disassembler.rs
+++ src/disassembler.rs
@@ -4,6 +4,36 @@ use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 
+/// Denotes the operand size of an instruction.
+///
+/// The value is determined by the highest two bits of
+/// the first byte of each instruction.
+#[derive(Debug)]
+pub enum OperandSize {
+    /// Denotes an operand size of 8-bit.
+    EightBit,
+    /// Denotes an operand size of 16-bit.
+    SixteenBit,
+    /// Denotes an operand size of 32-bit.
+    ThirtyTwoBit,
+
+    /// Denotes an unknown/variable operand size.
+    Unsized,
+}
+
+impl From<u8> for OperandSize {
+    fn from(first_byte: u8) -> Self {
+        // Check the highest two bits.
+        match first_byte >> 6 {
+            0b00 => OperandSize::EightBit,
+            0b01 => OperandSize::SixteenBit,
+            0b10 => OperandSize::ThirtyTwoBit,
+            0b11 => OperandSize::Unsized,
+            _ => panic!("Invalid instruction hit!"),
+        }
+    }
+}
+
 /// Reads the contents of a given binary file into a byte array.
 pub fn read_binary<P: AsRef<Path>>(path: P) -> Box<[u8]> {
     let mut binary_buffer = Vec::new();
