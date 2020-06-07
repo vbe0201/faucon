b61d5b62e0644f892fec7b34fdbde7f74c46285d
Valentin B <valentin.be@protonmail.com>
2020-06-07 13:29:14 +0200

    Redo the argument parsing for a more sane API

--- faucon-asm-macros/src/lib.rs
+++ faucon-asm-macros/src/lib.rs
@@ -9,7 +9,7 @@ use proc_macro2::Span;
 use quote::quote;
 use syn::{parse::Error, parse_macro_input, DeriveInput, Result};
 
-#[proc_macro_derive(Instruction, attributes(opcode, subopcode, operands))]
+#[proc_macro_derive(Instruction, attributes(insn))]
 pub fn instruction(input: TokenStream) -> TokenStream {
     // Parse input into a syntax tree.
     let ast = parse_macro_input!(input as DeriveInput);
@@ -22,88 +22,80 @@ fn impl_instruction(ast: &DeriveInput) -> Result<proc_macro2::TokenStream> {
     if let syn::Data::Enum(data) = &ast.data {
         let mut fields: Vec<proc_macro2::TokenStream> = Vec::new();
         for variant in data.variants.iter() {
-            let opcode = parse_int_arg(&extract_attribute(variant, "opcode")?)?;
-            let subopcode = parse_int_arg(&extract_attribute(variant, "subopcode")?)?;
-            let operands = parse_str_arg(&extract_attribute(variant, "operands")?)?;
+            let (opcode, subopcode, operands) = extract_insn_attributes(variant)?;
 
-            fields.push(quote! {
-                (#opcode, #subopcode) => Instruction::#variant.ident(#opcode, #subopcode, #operands)
-            });
+            panic!(format!("{} {} {}", opcode, subopcode, operands));
         }
 
-        Ok(quote! {
-            impl From<(u8, u8)> for #ast.ident {
-                fn from(op: (u8, u8)) -> Option<Self> {
-                    #(#fields),*
-                }
-            }
-        })
+        unimplemented!()
     } else {
         panic!("#[derive(Instruction)] can only be applied to enums")
     }
 }
 
-struct Attr {
-    pub ident: syn::Ident,
-    pub meta: syn::NestedMeta,
-}
-
-impl Attr {
-    fn new(ident: syn::Ident, meta: syn::NestedMeta) -> Self {
-        Attr { ident, meta }
-    }
-}
-
-fn extract_attribute(variant: &syn::Variant, name: &str) -> Result<Attr> {
-    if let Some(attribute) = variant
+fn extract_insn_attributes(variant: &syn::Variant) -> Result<(u8, u8, String)> {
+    if let Some(attr) = variant
         .attrs
         .iter()
-        .find(|a| a.path.segments.len() == 1 && a.path.segments[0].ident == name)
+        .find(|a| a.path.segments.len() == 1 && a.path.segments[0].ident == "insn")
     {
-        if let syn::Meta::List(ref nested_meta) = attribute.parse_meta()? {
-            if nested_meta.nested.len() == 1 {
-                Ok(Attr::new(
-                    attribute.path.segments[0].ident.clone(),
-                    nested_meta.nested[0].clone(),
-                ))
+        if let syn::Meta::List(ref nested_list) = attr.parse_meta()? {
+            if nested_list.nested.len() == 3 {
+                let mut arguments = Vec::new();
+
+                for nested_meta in nested_list.nested.iter() {
+                    if let syn::NestedMeta::Meta(syn::Meta::NameValue(ref value)) = nested_meta {
+                        arguments.push(value);
+                    } else {
+                        return Err(Error::new(
+                            attr.path.segments[0].ident.span(),
+                            "#[insn] is expecting its arguments in name=value format",
+                        ));
+                    }
+                }
+
+                let opcode = parse_int_arg(&arguments[0].lit)?;
+                let subopcode = parse_int_arg(&arguments[1].lit)?;
+                let operands = parse_str_arg(&arguments[2].lit)?;
+                Ok((opcode, subopcode, operands))
             } else {
                 Err(Error::new(
-                    attribute.path.segments[0].ident.span(),
-                    format!("\"{}\" is expected to be a single value", name),
+                    attr.path.segments[0].ident.span(),
+                    "#[insn] is expecting 3 arguments",
                 ))
             }
         } else {
             Err(Error::new(
-                attribute.path.segments[0].ident.span(),
-                format!("\"{}\" is expected to be a single value", name),
+                attr.path.segments[0].ident.span(),
+                "#[insn] is expecting arguments in list-style",
             ))
         }
     } else {
         Err(Error::new(
             Span::call_site(),
-            format!("attr \"{}\" missing", name),
+            "#[insn] attribute is missing",
         ))
     }
 }
 
-fn parse_int_arg(attr: &Attr) -> Result<u8> {
-    if let syn::NestedMeta::Lit(syn::Lit::Int(ref int)) = attr.meta {
+fn parse_int_arg(lit: &syn::Lit) -> Result<u8> {
+    if let syn::Lit::Int(ref int) = lit {
         Ok(int.base10_parse().unwrap())
     } else {
         Err(Error::new(
             Span::call_site(),
-            format!("\"{}\" is expected to be an int", attr.ident),
+            "Failed to parse the integer literal",
         ))
     }
 }
 
-fn parse_str_arg(attr: &Attr) -> Result<String> {
-    if let syn::NestedMeta::Lit(syn::Lit::Str(ref str)) = attr.meta {
+fn parse_str_arg(lit: &syn::Lit) -> Result<String> {
+    if let syn::Lit::Str(ref str) = lit {
         Ok(str.value())
     } else {
         Err(Error::new(
             Span::call_site(),
-            format!("\"{}\" is expected to be an int", attr.ident),
+            "Failed to parse the string literal",
         ))
     }
 }
