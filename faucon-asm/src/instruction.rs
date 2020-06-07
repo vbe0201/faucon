51db8029d9a78b4e45c36de8f0c94ba64019d369
Valentin B <valentin.be@protonmail.com>
2020-06-07 16:49:16 +0200

    Add InstructionKind enum to facilitate disassembling

---
+++ faucon-asm/src/instruction.rs
@@ -0,0 +1,27 @@
+use faucon_asm_macros::Instruction;
+
+use crate::operand::Operand;
+
+/// Assembly instructions that are supported by the Falcon ISA.
+///
+/// This enum is expanded by an internally used proc-macro which
+/// simplifies parsing instructions, given an `(opcode, subopcode)`
+/// pair. See the respective methods which ease up parsing work.
+#[derive(Clone, Debug, PartialEq, Eq, Instruction)]
+pub enum InstructionKind {
+    /// The XOR instruction.
+    ///
+    /// Applies a bitwise xor operation on two operands and stores
+    /// the result.
+    #[insn(opcode = 0xC0, subopcode = 6, operands = "R1D, R2S, I8")]
+    XOR(u8, u8, String),
+
+    /// An invalid or unknown instruction.
+    ///
+    /// This can be checked through calling [`InstructionKind::invalid`].
+    /// If such an instruction is encountered, the user may halt the
+    /// program.
+    ///
+    /// [`InstructionKind::invalid`]: struct.InstructionKind.html#method.invalid
+    XXX,
+}
--- faucon-asm/src/lib.rs
+++ faucon-asm/src/lib.rs
@@ -1,2 +1,3 @@
+pub mod instruction;
 pub mod opcode;
 pub mod operand;
