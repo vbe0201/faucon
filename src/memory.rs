cdb3e63e563be84f2119e3f4eb1113e11536d31c
Valentin B <valentin.be@protonmail.com>
2020-06-11 21:33:09 +0200

    Handle incorrect memory alignment in data space

--- src/memory.rs
+++ src/memory.rs
@@ -38,7 +38,10 @@ impl DataSpace {
     }
 
     /// Reads a halfword from a given address in memory.
-    pub fn read_halfword(&self, address: u32) -> u16 {
+    pub fn read_halfword(&self, mut address: u32) -> u16 {
+        // Enforce aligned memory access.
+        address &= !1;
+
         let low = self.read_byte(address) as u16;
         let high = self.read_byte(address + 1) as u16;
 
@@ -46,13 +49,24 @@ impl DataSpace {
     }
 
     /// Writes a halfword to a given address in memory.
-    pub fn write_halfword(&mut self, address: u32, value: u16) {
+    pub fn write_halfword(&mut self, mut address: u32, mut value: u16) {
+        // If the address is unaligned, fuck up the written value.
+        if address & 1 != 0 {
+            value = (value & 0xFF) << (address as u16 & 1) * 8;
+        }
+
+        // Enforce aligned memory access.
+        address &= !1;
+
         self.write_byte(address, value as u8);
         self.write_byte(address + 1, (value >> 8) as u8);
     }
 
     /// Reads a word from a given address in memory.
-    pub fn read_word(&self, address: u32) -> u32 {
+    pub fn read_word(&self, mut address: u32) -> u32 {
+        // Enforce aligned memory access.
+        address &= !3;
+
         let low = self.read_halfword(address) as u32;
         let high = self.read_halfword(address + 2) as u32;
 
@@ -60,7 +74,17 @@ impl DataSpace {
     }
 
     /// Writes a word to a given address in memory.
-    pub fn write_word(&mut self, address: u32, value: u32) {
+    pub fn write_word(&mut self, mut address: u32, mut value: u32) {
+        // If the address is unaligned, fuck up the written value.
+        if address & 1 != 0 {
+            value = (value & 0xFF) << (address & 3) * 8;
+        } else if address & 2 != 0 {
+            value = (value & 0xFFFF) << (address & 3) * 8;
+        }
+
+        // Enforce aligned memory access.
+        address &= !3;
+
         self.write_halfword(address, value as u16);
         self.write_halfword(address + 2, (value >> 16) as u16);
     }
