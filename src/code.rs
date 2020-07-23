1dfb2dbecd9b1d04ab4cf8dbc5ebe0058dcefe3c
Valentin B <valentin.be@protonmail.com>
2020-07-23 12:45:02 +0200

    Implement Falcon code upload properly

---
+++ src/code.rs
@@ -0,0 +1,79 @@
+//! Helpers for reading and uploading executables into the Falcon code segment.
+
+use std::convert::TryInto;
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+
+use faucon_emu::cpu::Cpu;
+
+const CODE_ALIGN_BITS: usize = 8;
+const CODE_ALIGNMENT: usize = 1 << CODE_ALIGN_BITS;
+
+fn align_up(value: usize, align: usize) -> usize {
+    assert!(align.is_power_of_two());
+
+    let mask = align - 1;
+    if value & mask == 0 {
+        value
+    } else {
+        (value | mask) + 1
+    }
+}
+
+fn read_file<P: AsRef<Path>>(path: P) -> Vec<u8> {
+    let mut file = File::open(path).expect("Failed to open the binary file");
+    let mut contents = Vec::new();
+
+    file.read_to_end(&mut contents).unwrap();
+
+    contents
+}
+
+/// Reads a binary at the given path and pads it out to 0x100 byte alignment, if
+/// necessary.
+///
+/// This function guarantees that the received slice of bytes can be mapped
+/// into the Falcon IMem correctly. Thus, any code to be uploaded to the Falcon
+/// should always be passed through this function.
+pub fn read_falcon_binary<P: AsRef<Path>>(path: P) -> Box<[u8]> {
+    let mut binary = read_file(path);
+
+    // Falcon code segment can only be modified in 0x100 byte pages.
+    // If the binary doesn't have correct alignment, pad it out so
+    // the page can be mapped correctly.
+    let aligned_len = align_up(binary.len(), CODE_ALIGNMENT);
+    while binary.len() < aligned_len {
+        binary.push(0);
+    }
+
+    binary.into_boxed_slice()
+}
+
+/// Uploads a Falcon binary that was obtained from [`read_falcon_binary`] into the
+/// code segment of the processor.
+///
+/// [`read_falcon_binary`]: fn.read_falcon_binary.html
+pub fn upload_to_imem(cpu: &mut Cpu, address: u16, vaddress: u32, binary: &[u8]) {
+    assert_eq!((address & 0xFC), 0);
+    assert_eq!((vaddress & 0xFC), 0);
+
+    for (i, page) in binary.chunks(CODE_ALIGNMENT).enumerate() {
+        upload_page_to_imem(
+            cpu,
+            address + (i << CODE_ALIGN_BITS) as u16,
+            vaddress + (i << CODE_ALIGN_BITS) as u32,
+            page,
+        );
+    }
+}
+
+fn upload_page_to_imem(cpu: &mut Cpu, address: u16, vaddress: u32, page: &[u8]) {
+    for (offset, word) in page.chunks(4).enumerate() {
+        cpu.upload_code(
+            address + (offset << 2) as u16,
+            vaddress,
+            u32::from_le_bytes(word.try_into().unwrap()),
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,49 +3,21 @@
 #[macro_use]
 extern crate nom;
 
-use std::convert::TryInto;
 use std::env;
-use std::fs::File;
-use std::io::Read;
-use std::path::Path;
 
 use debugger::Debugger;
 use faucon_emu::cpu::Cpu;
 
 #[macro_use]
 mod macros;
+mod code;
 mod debugger;
 
-/// Reads out the contents of a given binary file.
-fn read_binary<P: AsRef<Path>>(path: P) -> Box<[u8]> {
-    let mut file = File::open(path).expect("Failed to open the binary");
-    let mut buffer = Vec::new();
-
-    file.read_to_end(&mut buffer).unwrap();
-    while buffer.len() < 0x100 && buffer.len() % 4 != 0 {
-        buffer.push(0);
-    }
-
-    buffer.into_boxed_slice()
-}
-
-/// Uploads the contents of a binary to the Falcon IMEM.
-fn upload_code(cpu: &mut Cpu, address: u16, vaddress: u32, binary: &[u8]) {
-    for (index, chunk) in binary.chunks(4).enumerate() {
-        cpu.upload_code(
-            address + (index << 2) as u16,
-            vaddress,
-            u32::from_le_bytes(chunk.try_into().unwrap()),
-        );
-    }
-}
-
 fn main() {
-    let arguments = env::args().collect::<Vec<String>>();
-    let binary = read_binary(&arguments[1]);
+    let binary = code::read_falcon_binary(env::args().nth(1).unwrap());
 
     let mut cpu = Cpu::new();
-    upload_code(&mut cpu, 0, 0, &binary);
+    code::upload_to_imem(&mut cpu, 0, 0, &binary);
 
     let mut debugger = Debugger::new(cpu);
     debugger.run();
