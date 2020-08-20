eae10343be02e3b64ad8074ee91d8c736bbac873
Valentin B <valentin.be@protonmail.com>
2020-08-21 00:38:12 +0200

    Fix bug where debugger fails to parse decimal 0

--- src/debugger/commands.rs
+++ src/debugger/commands.rs
@@ -1,7 +1,7 @@
 use std::borrow::Cow;
 use std::str::FromStr;
 
-use nom::character::complete::{digit1, hex_digit1, space1};
+use nom::character::complete::{digit1, digit0, hex_digit1, space1};
 
 /// Commands that can be executed by the Falcon debugger.
 #[derive(Clone, Copy, Debug, PartialEq)]
@@ -98,7 +98,7 @@ named!(
     integer<&str, u32>,
     alt!(
         preceded!(
-            tag!("0x"),
+            complete!(tag!("0x")),
             map_res!(hex_digit1, |num: &str| u32::from_str_radix(&num[..], 16))
         )
         | flat_map!(digit1, parse_to!(u32))
