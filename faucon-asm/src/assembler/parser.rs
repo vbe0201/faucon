6baa20a0cf32d9b79ea9114ce0d93dcf4f6bb6b8
Valentin B <valentin.be@protonmail.com>
2021-02-22 19:53:12 +0100

    Implement more generic hex parsing strategy

--- faucon-asm/src/assembler/parser.rs
+++ faucon-asm/src/assembler/parser.rs
@@ -1,12 +1,30 @@
-use nom::character::complete::hex_digit1;
+use nom::branch::*;
+use nom::bytes::complete::*;
+use nom::character::complete::*;
+use nom::combinator::*;
+use nom::multi::*;
+use nom::sequence::*;
+use nom::IResult;
+use num_traits::{PrimInt, Unsigned};
 
-named!(
-    hex_u8<&str, u8>,
-    preceded!(
-        complete!(tag_no_case!("0x")),
-        map_res!(hex_digit1, |num: &str| u8::from_str_radix(&num[..], 16))
-    )
-);
+fn unsigned_hexadecimal<T>(input: &str) -> IResult<&str, T>
+where
+    T: PrimInt + Unsigned,
+{
+    map_res(
+        preceded(
+            opt(tag("+")),
+            preceded(
+                complete(tag_no_case("0x")),
+                recognize(many1(terminated(
+                    one_of("0123456789abcdefABCDEF"),
+                    many0(char('_')),
+                ))),
+            ),
+        ),
+        |out: &str| T::from_str_radix(&str::replace(&out, "_", ""), 16),
+    )(input)
+}
 
 #[cfg(test)]
 mod tests {
@@ -14,20 +32,55 @@ mod tests {
 
     #[test]
     fn parse_hex_u8() {
-        assert_eq!(hex_u8("0x12"), Ok(("", 0x12)));
-        assert_eq!(hex_u8("0X42"), Ok(("", 0x42)));
-        assert_eq!(hex_u8("0x53"), Ok(("", 0x53)));
-        assert_eq!(hex_u8("0XA5"), Ok(("", 0xA5)));
-        assert_eq!(hex_u8("0xFF"), Ok(("", 0xFF)));
+        assert_eq!(unsigned_hexadecimal::<u8>("0x12"), Ok(("", 0x12)));
+        assert_eq!(unsigned_hexadecimal::<u8>("+0X42"), Ok(("", 0x42)));
+        assert_eq!(unsigned_hexadecimal::<u8>("+0x53"), Ok(("", 0x53)));
+        assert_eq!(unsigned_hexadecimal::<u8>("0XA5"), Ok(("", 0xA5)));
+        assert_eq!(unsigned_hexadecimal::<u8>("0xFF"), Ok(("", 0xFF)));
 
-        assert_eq!(hex_u8("0x5 aaa"), Ok((" aaa", 0x5)));
-        assert_eq!(hex_u8("0xC2 !@b"), Ok((" !@b", 0xC2)));
+        assert_eq!(unsigned_hexadecimal::<u8>("0x5 aaa"), Ok((" aaa", 0x5)));
+        assert_eq!(unsigned_hexadecimal::<u8>("+0xC2 !@b"), Ok((" !@b", 0xC2)));
 
-        assert_eq!(hex_u8("0x0000"), Ok(("", 0x0)));
-        assert_eq!(hex_u8("0X0001"), Ok(("", 0x1)));
-        assert_eq!(hex_u8("0x0010"), Ok(("", 0x10)));
+        assert_eq!(unsigned_hexadecimal::<u8>("0x0000"), Ok(("", 0x0)));
+        assert_eq!(unsigned_hexadecimal::<u8>("0X0001"), Ok(("", 0x1)));
+        assert_eq!(unsigned_hexadecimal::<u8>("+0x0010"), Ok(("", 0x10)));
 
-        assert_ne!(hex_u8("-0x0").is_ok(), true);
-        assert_ne!(hex_u8("0x100").is_ok(), true);
+        assert_eq!(unsigned_hexadecimal::<u8>("0x100").is_ok(), false);
+    }
+
+    #[test]
+    fn parse_hex_u16() {
+        assert_eq!(unsigned_hexadecimal::<u16>("0x0"), Ok(("", 0x0)));
+        assert_eq!(unsigned_hexadecimal::<u16>("+0xDEAD"), Ok(("", 0xDEAD)));
+        assert_eq!(unsigned_hexadecimal::<u16>("0XFFFF"), Ok(("", 0xFFFF)));
+
+        assert_eq!(
+            unsigned_hexadecimal::<u16>("0x5487 abc"),
+            Ok((" abc", 0x5487))
+        );
+        assert_eq!(
+            unsigned_hexadecimal::<u16>("0XF00D j%$"),
+            Ok((" j%$", 0xF00D))
+        );
+
+        assert_eq!(unsigned_hexadecimal::<u16>("0x0000_FFFF"), Ok(("", 0xFFFF)));
+        assert_eq!(unsigned_hexadecimal::<u16>("+0xA_B_C_D"), Ok(("", 0xABCD)));
+
+        assert_eq!(unsigned_hexadecimal::<u16>("0x10000").is_ok(), false);
+    }
+
+    #[test]
+    fn parse_hex_u32() {
+        assert_eq!(unsigned_hexadecimal::<u32>("+0x0"), Ok(("", 0x0)));
+        assert_eq!(
+            unsigned_hexadecimal::<u32>("0xDEADBEEF"),
+            Ok(("", 0xDEADBEEF))
+        );
+        assert_eq!(
+            unsigned_hexadecimal::<u32>("+0XDEAD_BEEF"),
+            Ok(("", 0xDEADBEEF))
+        );
+
+        assert_eq!(unsigned_hexadecimal::<u32>("0x1_0000_0000").is_ok(), false);
     }
 }
