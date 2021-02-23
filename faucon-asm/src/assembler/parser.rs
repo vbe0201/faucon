4b3858de32d43e8395c3c2e2eb874aa59b9dce1d
Valentin B <valentin.be@protonmail.com>
2021-02-23 19:36:34 +0100

    Add support for parsing octal literals

--- faucon-asm/src/assembler/parser.rs
+++ faucon-asm/src/assembler/parser.rs
@@ -55,3 +55,46 @@ where
         |out: &str| T::from_str_radix(&str::replace(&out, "_", ""), 16),
     )(input)
 }
+
+fn signed_octal<T>(input: &str) -> IResult<&str, T>
+where
+    T: PrimInt + Signed,
+{
+    map_res(
+        pair(
+            map(opt(alt((tag("+"), tag("-")))), |sign: Option<&str>| {
+                sign.map(|s| if s == "-" { true } else { false })
+                    .unwrap_or(false)
+            }),
+            preceded(
+                complete(tag_no_case("0o")),
+                recognize(many1(terminated(one_of("01234567"), many0(char('_'))))),
+            ),
+        ),
+        |out: (bool, &str)| {
+            T::from_str_radix(&str::replace(&out.1, "_", ""), 8).and_then(|n| {
+                if out.0 {
+                    Ok(-n)
+                } else {
+                    Ok(n)
+                }
+            })
+        },
+    )(input)
+}
+
+fn unsigned_octal<T>(input: &str) -> IResult<&str, T>
+where
+    T: PrimInt + Unsigned,
+{
+    map_res(
+        preceded(
+            opt(tag("+")),
+            preceded(
+                complete(tag_no_case("0o")),
+                recognize(many1(terminated(one_of("01234567"), many0(char('_'))))),
+            ),
+        ),
+        |out: &str| T::from_str_radix(&str::replace(&out, "_", ""), 8),
+    )(input)
+}
