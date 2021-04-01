1c1248230ccde8548e96c82401127254a5293b70
Valentin B <valentin.be@protonmail.com>
2021-04-01 14:01:06 +0200

    Don't insert padding when counter already exceeds the size value

--- faucon-asm/src/assembler/codegen.rs
+++ faucon-asm/src/assembler/codegen.rs
@@ -281,8 +281,10 @@ fn lower_directive<'a>(output: &mut Vec<u8>, section: &mut Section<'a>, directiv
             section.counter += size_of::<u32>() as u32;
         }
         Directive::Size(size) => {
-            skip(output, size - section.counter, 0);
-            section.counter = size;
+            if section.counter < size {
+                skip(output, size - section.counter, 0);
+                section.counter = size;
+            }
         }
         Directive::Skip(size, value) => {
             skip(output, size, value.unwrap_or(0));
